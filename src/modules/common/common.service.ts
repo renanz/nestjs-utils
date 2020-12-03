44ca09c5ac3c417a0d6d8900743625c2581e7cab
Renán Zelaya <renan_zelaya@hotmail.com>
2020-12-03 13:27:59 -0600

    Add common service

---
+++ src/modules/common/common.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { Repository, FindOneOptions, DeleteResult, FindManyOptions, FindConditions } from 'typeorm';
+
+export type FindOneArguments<T> = [id: number, options?: FindOneOptions<T>] | [options?: FindOneOptions<T>];
+
+@Injectable()
+export class CommonService<TEntity> {
+  constructor(@InjectEntityManager() private readonly repository: Repository<TEntity>) {}
+
+  public async find(options?: FindConditions<TEntity> | FindManyOptions<TEntity>): Promise<TEntity[]> {
+    return await this.repository.find(options);
+  }
+
+  public async findOne(...[paramOne, options]: FindOneArguments<TEntity>): Promise<TEntity | undefined> {
+    if (typeof paramOne === 'number') {
+      return await this.repository.findOne(paramOne, options);
+    }
+    return await this.repository.findOne(paramOne);
+  }
+
+  public async findOneOrFail(...[paramOne, options]: FindOneArguments<TEntity>): Promise<TEntity> {
+    if (typeof paramOne === 'number') {
+      return await this.repository.findOneOrFail(paramOne, options);
+    }
+    return await this.repository.findOneOrFail(paramOne);
+  }
+
+  public async count(options?: FindManyOptions<TEntity>) {
+    return await this.repository.count(options);
+  }
+
+  public async save(entity: TEntity): Promise<TEntity> {
+    return await this.repository.save(entity);
+  }
+
+  async remove(id: number): Promise<DeleteResult> {
+    return await this.repository.delete(id);
+  }
+}
